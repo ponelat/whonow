b3c5411612eede777a5e0a84b3e03d50d09420b7
Josh Ponelat <jponelat@gmail.com>
2020-10-13 17:44:22 +0200

    add some options and forwarding

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const dns    = require('native-dns')
 const log    = require('loglevel')
 const c      = require('chalk')
 const { ArgumentParser } = require('argparse')
+const SERVFAIL = require('native-dns-packet').consts.NAME_TO_RCODE.SERVFAIL;
 
 const DomainRebind = require('./src/DomainRebind.js')
 const server = dns.createServer()
@@ -32,6 +33,20 @@ function main() {
 
     server.on('request', (request, response) => {
 
+      // Should we forward the request?
+      // TODO: Improve the endsWith logic, to not greedily match things like,  foo{baseDomain}.com.
+      if(!request.question[0].name.toLowerCase().endsWith(`${args['base_domain']}`) && args.forward) {
+        return forwardTo(request.question[0], args.forward).then(({rcode, answer, additional, authority}) => {
+          logAnswer(answer, request)
+          response.answer = answer
+          response.additional = additional
+          response.authority = authority
+          response.header.rcode = rcode
+
+          response.send()
+        })
+      }
+
         request.question.forEach(question => {
 
             // only handle A queries for now
@@ -55,7 +70,7 @@ function main() {
                 let answer = {
                     name: question.name, // respond with original name
                     address: address || args['default_answer'],
-                    ttl: 1
+                    ttl: parseInt(args.ttl)
                 }
 
                 response.answer.push(dns.A(answer))
@@ -63,41 +78,7 @@ function main() {
         })
 
         if (response.answer.length > 0) {
-            response.answer.forEach(ans => {
-
-                const src = `${request.address.address}:${request.address.port}`
-
-                if (args.logfile) {
-
-                    // 'timestamp,src_ip,question_type,question,answer'
-                    let line = Date.now() + ','
-                    line += src + ','
-                    line += 'A,'
-                    line += ans.name + ','
-                    line += ans.address + '\n'
-
-                    // WARNING: because these calls are async, requests may be
-                    // logged out of order. This shouldn't be a problem though
-                    // as the csv can be sorted by timestamp
-                    fs.appendFile(args.logfile, line, (err) => {
-                        if (err) {
-                            log.error(c.red('[!]') + ` error writing to ${args.logfile}`)
-                            log.error(err)
-                        }
-                    })
-                }
-
-                if (args.verbose) {
-                    // time in ISO date format string
-                    // https://docs.microsoft.com/en-us/scripting/javascript/date-and-time-strings-javascript#iso-date-format
-                    const time = c.yellow(new Date().toISOString())
-                    const answer = c.cyan(ans.address.padEnd(15))
-                    const address = c.magenta(src.padEnd(21))
-                    log.info(c.blue('[+]') + ` ${time} ${address} A ${answer} ${ans.name}`)
-                } else {
-                    log.info(c.blue('[+]') + ` A ${c.cyan(ans.address.padEnd(15))} ${ans.name}`)
-                }
-            })
+          logAnswer(response.answer, request)
         } else {
             // silently responding to non-A requests with an empty response packet
             // could print here for debugging
@@ -107,7 +88,11 @@ function main() {
     })
 
     server.on('listening', () => {
-        log.info(c.gray('[*]') + ` server listening on port ${args.port}`)
+        log.info(c.gray('[*]') + ` server listening on port ${server.address().address} on ${server.address().port}`)
+        if(args.forward)
+          log.info(c.gray('[*]') + ` forwarding DNS questions to ${args.forward}`)
+        if(args['base_domain'])
+          log.info(c.gray('[*]') + ` capturing domains that end with ${args["base_domain"]}`)
     })
 
     server.on('error', (err, buff, req, res) => {
@@ -120,10 +105,52 @@ function main() {
             let m = c.red('[!]')
             m += ` Fatal error binding to port ${args.port}, address in use.`
             log.error(m)
+        } else {
+            log.error(`Failed with: {err.message}`)
         }
     })
 
-    server.serve(args.port)
+  server.serve(args.port, args.address)
+
+  function logAnswer(answer, request) {
+    if(!Array.isArray(answer))
+      return
+    answer.forEach(ans => {
+      const src = `${request.address.address}:${request.address.port}`
+
+      if (args.logfile) {
+
+        // 'timestamp,src_ip,question_type,question,answer'
+        let line = Date.now() + ','
+        line += src + ','
+        line += 'A,'
+        line += ans.name + ','
+        line += ans.address + '\n'
+
+        // WARNING: because these calls are async, requests may be
+        // logged out of order. This shouldn't be a problem though
+        // as the csv can be sorted by timestamp
+        fs.appendFile(args.logfile, line, (err) => {
+          if (err) {
+            log.error(c.red('[!]') + ` error writing to ${args.logfile}`)
+            log.error(err)
+          }
+        })
+      }
+
+      if (args.verbose) {
+        // time in ISO date format string
+        // https://docs.microsoft.com/en-us/scripting/javascript/date-and-time-strings-javascript#iso-date-format
+        const time = c.yellow(new Date().toISOString())
+        const answer = c.cyan((ans.address || '').padEnd(15))
+        const address = c.magenta((src || '').padEnd(21))
+        log.info(c.blue('[+]') + ` ${time} ${address} A ${answer} ${ans.name}`)
+      } else {
+        log.info(c.blue('[+]') + ` A ${c.cyan((ans.address || '').padEnd(15))} ${ans.name}`)
+      }
+    })
+
+  }
 }
 
 function parseArgs() {
@@ -140,6 +167,46 @@ function parseArgs() {
       {
         help: 'What port to run the DNS server on (default: 53).',
         defaultValue: 53
+      }
+    )
+
+    parser.addArgument(
+      [ '-a', '--address' ],
+      {
+        help: 'What address to run the DNS server on (default: 127.0.0.1).',
+        defaultValue: '127.0.0.1'
+      }
+    )
+
+    parser.addArgument(
+      [ '-n', '--base-domain' ],
+      {
+        help: 'What domain to respond to (default: example.local).',
+        defaultValue: 'example.local'
+      }
+    )
+
+    parser.addArgument(
+      [ '-t', '--ttl' ],
+      {
+        help: 'How long a TTL to give to answer that we give (default: 1)',
+        defaultValue: '1',
+      }
+    )
+
+    parser.addArgument(
+      [ '-f', '--forward' ],
+      {
+        help: 'What address to forward unknown questions to (default: none, but try 8.8.8.8).',
+        defaultValue: ''
+      }
+    )
+
+    parser.addArgument(
+      [ '-i', '--forward-filter' ],
+      {
+        help: 'A filter to decide which questions to forward (default: none)',
+        defaultValue: ''
       }
     )
 
@@ -180,6 +247,43 @@ function parseArgs() {
         }
     )
     return parser.parseArgs()
+}
+
+function forwardTo(question, forwardAddress) {
+  return new Promise((resolve, reject) => {
+    let responseObj = {}
+    // Forwarding
+    var innerRequest = dns.Request({
+      question,
+      server: {
+        address: forwardAddress,
+        type: 'udp',
+        port: 53,
+      },
+      cache: false,
+    })
+
+    innerRequest.send();
+
+    // in the event we get an error or timeout paper over with servfail
+    responseObj.rcode = SERVFAIL;
+
+    innerRequest.on('message', function (err, innerResponse) {
+      responseObj.rcode = innerResponse.header.rcode
+      responseObj.answer = innerResponse.answer
+      responseObj.additional = innerResponse.additional
+      responseObj.authority = innerResponse.authority
+    });
+
+    innerRequest.on('error', function(err) {
+      reject(err)
+    })
+
+    innerRequest.on('end', function() {
+      resolve(responseObj)
+    })
+
+  })
 }
 
 main()
